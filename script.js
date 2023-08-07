c1e02444428c3d0d82121abc2149e4902b3f868b
Vicktor007 <vicktord007@gmail.com>
2023-08-07 11:14:35 +0100

    save

--- script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("scroll", function(){
-    const scroll = document.querySelector('.top');
-    scroll.classList.toggle("active", window.scrollY > 500)
-})
-
-window.addEventListener("scroll", function(){
-    const scroll = document.querySelector('.bottom');
-    scroll.classList.toggle("active", window.scrollY < 500)
-})
